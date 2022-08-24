3e36bbf81c336a04f78f4a36473ad286ea0fb1d6
Gilberto Barbosa <gisantos555@gmail.com>
2022-08-24 12:48:40 -0300

    Projeto inicial

---
+++ src/Helper/EntityManagerCreator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alura\Doctrine\Helper;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+class EntityManagerCreator
+{
+    public static function createEntityManager(): EntityManager
+    {
+        // Create a simple "default" Doctrine ORM configuration for Annotations
+        
+        $config = ORMSetup::createAnnotationMetadataConfiguration(
+            array(__DIR__."/src"), 
+            true
+        );
+        // or if you prefer YAML or XML
+        // $config = ORMSetup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+
+        // database configuration parameters
+        $conn = [
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__ . '/../../db.sqlite',
+        ];
+
+        // obtaining the entity manager
+        return EntityManager::create($conn, $config);
+    }
+}
